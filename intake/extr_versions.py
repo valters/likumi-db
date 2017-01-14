bd512837e30850de3227b017cc7d9811f172c7eb
Valters Vingolds <valters@vingolds.ch>
2017-01-14 14:00:09 +0100

    intake: versions .ver files generated

--- intake/extr_versions.py
+++ intake/extr_versions.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 
-import lxml.html
-import json
-import sys
+""" list all .html files in folder, and generate .ver files for them """
 
-docfile = 'satversme' if len(sys.argv) == 1 else sys.argv[1]
-print(docfile)
+import os
 
-root = lxml.html.parse(docfile+'.html')
-versions_el = root.xpath("//div[@id='ver_date']")
-versions_text = versions_el[0].text_content()
-print(versions_text)
+import write_versions
 
-out = open(docfile+'.ver', 'w')
-versions = json.loads(versions_text)
-for ver in versions['data']:
-	print(ver['value'], file=out)
+files = [ f for f in os.listdir( '.' ) if os.path.isfile( f ) ]
+for f in files:
+    print( f, end='' )
 
-out.close()
+    fname, ext = os.path.splitext( os.path.basename(f) )
+    if ext == '.html':
+        print( ': ', end='' )
+        try:
+            write_versions.write_versions_file( fname, write_versions.parse_versions( fname ) )
+            print('ok')
+        except RuntimeError as e:
+            print( "error: " + str(e))
+    else:
+        print(' (ignored)')
---
+++ intake/write_versions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+""" extract version info from likumi dump, write .ver file """
+
+import lxml.html
+import json
+import sys
+
+DEBUG=0
+
+def parse_versions(docfile):
+    root = lxml.html.parse(docfile+'.html')
+    versions_el = root.xpath("//div[@id='ver_date']")
+    versions_text = versions_el[0].text_content()
+    print(versions_text) if DEBUG else None
+    return versions_text
+
+def write_versions_file(docname,versions_text):
+    out = open(docname+'.ver', 'w')
+    try:
+        versions = json.loads(versions_text)
+
+        for ver in versions['data']:
+            print(ver['value'], file=out)
+    except json.decoder.JSONDecodeError as e:
+        raise RuntimeError("failed to write file '{}': error occurred: ".format(docname) + str(e))
+    finally:
+        out.close()
+
+if __name__ == '__main__':
+    """ if testing: provide name of file (without extension) to process single likumi file """
+    DEBUG=1
+    docfile = 'satversme' if len(sys.argv) == 1 else sys.argv[1]
+    print(docfile)
+    write_versions_file(docfile, parse_versions(docfile))
